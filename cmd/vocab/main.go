f4f6687e37544ef367f34e68bc783cc773b5d69c
audetv <audetv@gmail.com>
2025-02-16 00:17:21 +0300

    Добавлено профилирование

--- cmd/vocab/main.go
+++ cmd/vocab/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"net/http"
+	_ "net/http/pprof" // Импортируем pprof
 	"os"
 	"runtime"
+	"time"
 
 	"github.com/terratensor/vocab/internal/tokenizer"
 )
@@ -17,12 +20,24 @@ func main() {
 	dirPath := flag.String("dir", "./files", "Path to the directory containing text files")
 	maxGoroutines := flag.Int("max-goroutines", 0, "Maximum number of goroutines (default: number of CPUs)")
 	outputFile := flag.String("output", "vocab.txt", "Output file for the vocabulary")
+	pprofFlag := flag.Bool("pprof", false, "Enable pprof profiling")
 	flag.Parse()
 
 	// Если maxGoroutines не указан, используем количество процессоров
 	if *maxGoroutines <= 0 {
 		*maxGoroutines = runtime.NumCPU()
 		fmt.Printf("Using %d goroutines (number of CPUs)\n", *maxGoroutines)
+	}
+
+	// Включение pprof
+	if *pprofFlag {
+		go func() {
+			fmt.Println("Starting pprof server on http://localhost:6060")
+			if err := http.ListenAndServe("localhost:6060", nil); err != nil {
+				fmt.Printf("Error starting pprof server: %v\n", err)
+			}
+		}()
+		time.Sleep(1 * time.Second) // Даем время для запуска сервера
 	}
 
 	// Создание токенизатора
